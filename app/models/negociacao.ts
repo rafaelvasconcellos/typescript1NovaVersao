f1bd6710a9919e2c56b2c519efe36cd97acfe3ef
Rafael Vasconcellos <rfl.vasconcellos@gmail.com>
2021-10-28 18:15:11 -0300

    Refatoração do construtor

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,30 +1,25 @@
 export class Negociacao {
 
-    private _data: Date;
-    private _quantidade: number;
-    private _valor: number;
-
-    constructor(data: Date, quantidade: number, valor: number) {
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
-    }
+    constructor(
+        private _data: Date,
+        private _quantidade: number,
+        private _valor: number) { }
 
 
-    get data() {
+    get data(): Date {
         return this._data;
     }
 
-    get quantidade() {
+    get quantidade(): number {
         return this._quantidade;
     }
 
-    get valor() {
+    get valor(): number {
         return this._valor;
     }
 
 
-    get volume(){
+    get volume(): number {
         return this._quantidade * this._valor;
     }
 }
